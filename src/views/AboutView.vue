47d15a09d56ae126e32562d29f42d7b0b0813ed9
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2022-05-12 00:59:53 -0300

    adds class syntax

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="decrement">-</button>
+    {{ count }}
+    <button @click="increment">+</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Counter extends Vue {
+  count = 0
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
+}
+</script>

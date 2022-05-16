10f0a20e15ed1b96888086c45f7c778630b093a7
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2022-05-15 22:48:27 -0300

    adds date picker

---
+++ src/components/molecules/DatePickerMolecule.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="date"
+          label="Date"
+          append-outer-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        />
+      </template>
+      <v-date-picker v-model="date" @input="changeDate" />
+    </v-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class DatePickerMolecule extends Vue {
+  public menu = false;
+  public date = new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+    .toISOString()
+    .substring(0, 10);
+
+  public changeDate(): void {
+    this.menu = false;
+    this.$emit('input', this.date);
+  }
+}
+</script>
--- src/components/organisms/TaskCreatorFormOrganism.vue
+++ src/components/organisms/TaskCreatorFormOrganism.vue
@@ -1,17 +1,20 @@
 <template>
   <v-form ref="form">
     <v-text-field
+      class="mt-4"
       v-model="title"
       :counter="maxTitleSize"
       :rules="titleRules"
       label="Title"
     />
     <v-textarea
+      class="mt-4"
       v-model="description"
       :counter="maxDescriptionSize"
       :rules="descriptionRules"
       label="Description"
     />
+    <DatePickerMolecule class="mt-10 mb-2" @input="changeDate" />
     <v-btn :color="submitColor" dark class="mr-4" @click="submit">submit</v-btn>
     <v-btn :color="clearColor" @click="clear" dark>clear</v-btn>
   </v-form>
@@ -22,8 +25,13 @@ import { Colors } from '@/models/constants/Colors';
 import { TaskService } from '@/services/TaskService';
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import DatePickerMolecule from '../molecules/DatePickerMolecule.vue';
 
-@Component
+@Component({
+  components: {
+    DatePickerMolecule,
+  },
+})
 export default class TaskCreatorFormOrganism extends Vue {
   private taskService = new TaskService();
   public clearColor = Colors.LightBlue;
@@ -32,6 +40,7 @@ export default class TaskCreatorFormOrganism extends Vue {
   public maxTitleSize = 30;
   public maxDescriptionSize = 5000;
   public description = '';
+  public date = new Date();
 
   public titleRules = [
     (v: any) =>
@@ -54,9 +63,19 @@ export default class TaskCreatorFormOrganism extends Vue {
     return (this.$refs.form as any).validate();
   }
 
+  public changeDate(dateString: string): void {
+    const date = new Date(dateString);
+    const userTimezoneOffset = date.getTimezoneOffset() * 60000;
+    this.date = new Date(date.getTime() + userTimezoneOffset);
+  }
+
   public submit(): void {
     if (this.validateField()) {
-      this.taskService.addTask(this.title, this.description, new Date());
+      this.taskService.addTask(
+        this.title,
+        this.description,
+        this.date ?? new Date(),
+      );
       this.clear();
     }
   }

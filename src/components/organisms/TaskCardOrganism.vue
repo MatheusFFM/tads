fe18e448b485943051c0d340fc5ce7e83b77540b
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2022-05-15 23:10:01 -0300

    remove click on icon button

--- src/components/organisms/TaskCardOrganism.vue
+++ src/components/organisms/TaskCardOrganism.vue
@@ -25,7 +25,7 @@
     >
       <v-spacer></v-spacer>
 
-      <v-btn icon @click="show = !show">
+      <v-btn icon>
         <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
       </v-btn>
     </v-card-actions>
